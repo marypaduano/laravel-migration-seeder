12e8392392bb757f8b55040f4c14ebb9f78ca50a
marypaduano <maria.paduano@gmail.com>
2023-04-13 18:37:46 +0200

    FIX card style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,18 +3,50 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <h1 class="text-center">Calendario Treni</h1>
 
         @foreach ($trains as $train)
             <div class="col-3">
-                <h3>Azienda: {{ $train->azienda }}</h3>
-                <h5>Stazione di Partenza: {{ $train->stazione_di_partenza }}</h5>
-                <h5>Stazione di Arrivo: {{ $train->stazione_di_arrivo }}</h5>
-                <span>Orario di Partenza: {{ $train->orario_di_partenza }}</span>
-                <span>Orario di Arrivo: {{ $train->orario_di_arrivo }}</span>
-                <p>Codice Treno: {{ $train->codice_treno }}</p>
-                <p>Numero Carrozze: {{ $train->numero_carrozze }}</p>
+                <ul class="card py-3" style="list-style: none;">
+                    <li class="card-title">
+                        <span>Azienda:
+                            <strong>{{ $train->azienda }}</strong>
+                        </span> 
+                    </li>
+                    <li>
+                        <span>Stazione di Partenza:
+                            <strong>{{ $train->stazione_di_partenza }}</strong>
+                        </span>
+                    </li>
+                    <li>
+                        <span>Stazione di Arrivo:
+                            <strong>{{ $train->stazione_di_arrivo }}</strong>
+                        </span>
+                    </li>
+                    <li>
+                        <span>Orario di Partenza:
+                            <strong>{{ $train->orario_di_partenza }}</strong>
+                        </span>
+                    </li>
+                    <li>
+                        <span>Orario di Arrivo:
+                            <strong>{{ $train->orario_di_arrivo }}</strong> 
+                        </span>
+                    </li>
+                    <li>
+                        <p>Codice Treno: {{ $train->codice_treno }}</p>
+                    </li>
+                    <li>
+                        <p>Numero Carrozze: {{ $train->numero_carrozze }}</p>
+                    </li>
+                    <li>
+                        <p>in orario: {{ $train->in_orario }}</p>
+                    </li>
+                    <li>
+                        <p>cancellato: {{ $train->cancellato }}</p>
+                    </li>
+                </ul>
             </div>
-
         @endforeach
 
     </div>
