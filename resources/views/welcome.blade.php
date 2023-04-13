83037a530dfbd177f9643b501630b4dcb817c1f8
marypaduano <maria.paduano@gmail.com>
2023-04-13 16:47:48 +0200

    Fix Trains Stamp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,13 @@
 
         @foreach ($trains as $train)
             <div class="col-3">
-                <h3>{{ $train->azienda }}</h3>
-                <h5>{{ $train->stazione_di_partenza }}</h5>
-                <h5>{{ $train->stazione_di_arrivo }}</h5>
+                <h3>Azienda: {{ $train->azienda }}</h3>
+                <h5>Stazione di Partenza: {{ $train->stazione_di_partenza }}</h5>
+                <h5>Stazione di Arrivo: {{ $train->stazione_di_arrivo }}</h5>
                 <span>Orario di Partenza: {{ $train->orario_di_partenza }}</span>
                 <span>Orario di Arrivo: {{ $train->orario_di_arrivo }}</span>
-                <p>{{ $train->codice_treno }}</p>
-                <p>{{ $train->numero_carrozze }}</p>
+                <p>Codice Treno: {{ $train->codice_treno }}</p>
+                <p>Numero Carrozze: {{ $train->numero_carrozze }}</p>
             </div>
 
         @endforeach
